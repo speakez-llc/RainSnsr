2f3323d6599571e3943e43d16fc59719a1282bc4
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-06-24 14:11:08 -0400

    using new servo API

--- MeadowApp.fs
+++ MeadowApp.fs
@@ -1,13 +1,13 @@
 ﻿namespace RainSnsr
 
 open System
+open System.Threading.Tasks
 open Meadow
 open Meadow.Devices
 open Meadow.Foundation.Leds
 open Meadow.Foundation.Relays
 open Meadow.Foundation.Servos
 open Meadow.Hardware
-open System.Threading.Tasks
 open Meadow.Units
 
 type MeadowApp() =
@@ -18,15 +18,16 @@ type MeadowApp() =
     let mutable extendRelay : Relay = null
     let mutable lightRelay : Relay = null
     let mutable p2relay : Relay = null
+    let mutable pwmPort : IPwmPort = null
     let mutable rainSensor : IDigitalInputPort = null
-    let mutable wiperServo : Servo = null
+    let mutable wiperServo : AngularServo = null
     let mutable led : RgbPwmLed = null
     
     let toggleRelay (relay : Relay) (message : string) =
         async {
             Resolver.Log.Info(message)
             relay.Toggle()
-            do! Task.Delay(150) |> Async.AwaitTask
+            do! Async.Sleep 150
             relay.Toggle()
         }
     
@@ -46,29 +47,31 @@ type MeadowApp() =
     }
     
     let clearRainSnsr = async {
+        Resolver.Log.Info "Clearing Sensor..."
         let stopwatch = System.Diagnostics.Stopwatch()
-        do! Task.Delay(2000) |> Async.AwaitTask
         stopwatch.Start()
         while stopwatch.Elapsed.TotalSeconds < 5.0 do
-            do! wiperServo.RotateTo(Angle 95) |> Async.AwaitTask
-            do! wiperServo.RotateTo(Angle 180) |> Async.AwaitTask
-        do! Task.Delay(2000) |> Async.AwaitTask
+            do! Async.AwaitTask (Task.Run(fun () -> wiperServo.RotateTo(Angle 5)))
+            do! Async.AwaitTask (Task.Run(fun () -> wiperServo.RotateTo(Angle 90)))
+        Resolver.Log.Info "Sensor Clear Complete
+        ..."
+        do! Async.Sleep 2000
     }
         
     let runRainSnsrAsync = async {
-        do! wiperServo.RotateTo(Angle(0.0, Angle.UnitType.Degrees)) |> Async.AwaitTask
+        do! Async.AwaitTask (Task.Run(fun () -> wiperServo.RotateTo(Angle 0)))
         Resolver.Log.Info "Starting Rain Sensor..."
         while true do
             let rainState = getRainState |> Async.RunSynchronously
             if rainState then
                 do! toggleRelay retractRelay "Retracting awning..."
-                do! ShowColor Color.Aqua (TimeSpan.FromMilliseconds 500) 
-                do! wiperServo.RotateTo(Angle 180) |> Async.AwaitTask
-                clearRainSnsr |> Async.RunSynchronously
-                do! wiperServo.RotateTo(Angle 0) |> Async.AwaitTask          
-                do! Task.Delay(5000) |> Async.AwaitTask
+                do! ShowColor Color.Aqua (TimeSpan.FromMilliseconds 500)
+                do! Async.AwaitTask (Task.Run(fun () -> wiperServo.RotateTo(Angle 180)))
+                do! clearRainSnsr
+                do! Async.AwaitTask (Task.Run(fun () -> wiperServo.RotateTo(Angle 0)))
+                do! Async.Sleep 2000
             else 
-                do! Task.Delay(15000) |> Async.AwaitTask
+                do! Async.Sleep 15000
     }
 
     override this.Initialize() =
@@ -83,17 +86,11 @@ type MeadowApp() =
         lightRelay <- Relay(MeadowApp.Device.Pins.D06)
         p2relay <- Relay(MeadowApp.Device.Pins.D05)
         rainSensor <- MeadowApp.Device.Pins.D13.CreateDigitalInputPort(ResistorMode.ExternalPullDown)
-        let servoConfig = ServoConfig(
-            minimumAngle = Angle 0,
-            maximumAngle = Angle 180,
-            minimumPulseDuration = 1000,
-            maximumPulseDuration = 2406
-            )
-        wiperServo <- Servo(MeadowApp.Device.Pins.D12, servoConfig)
-
-        Task.CompletedTask;
-
-    override this.Run () =
-        task {
-            do! runRainSnsrAsync
-        } 
+        pwmPort <- MeadowApp.Device.CreatePwmPort(MeadowApp.Device.Pins.D12, Frequency 50)
+        wiperServo <- new AngularServo(pwmPort, AngularServo.PulseAngle(Angle 0, TimeSpan.FromMilliseconds 1.0),
+                                       AngularServo.PulseAngle(Angle 180, TimeSpan.FromMilliseconds 2.4))
+        
+        Task.CompletedTask
+    override this.Run () = task {
+        do! runRainSnsrAsync
+       }
